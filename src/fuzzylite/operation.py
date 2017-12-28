f884eaf427d05c388968eb395752531a25a3bd79
Juan Rada-Vilela <jcrada@fuzzylite.com>
2017-12-28 14:03:01 +1300

    Operation class

--- src/fuzzylite/operation.py
+++ src/fuzzylite/operation.py
@@ -15,24 +15,28 @@
  fuzzylite is a registered trademark of FuzzyLite Limited.
 """
 
-decimals = 3
 
+class Operation(object):
+    '''
+    Operation
+    '''
 
-def valid_name(name: str) -> str:
-    result = ''.join([x for x in name if x in ("_", ".") or x.isalnum()])
-    return result if result else "unnamed"
+    '''Decimals'''
+    decimals = 3
 
+    def valid_name(name: str) -> str:
+        result = ''.join([x for x in name if x in ("_", ".") or x.isalnum()])
+        return result if result else "unnamed"
 
-def str_(x: float) -> str:
-    global decimals
-    return ("{:.%sf}" % decimals).format(x)
+    @staticmethod
+    def str(x) -> str:
+        # todo: if x is float, int or str
+        return ("{:.%sf}" % Operation.decimals).format(x)
 
+    def scale(x: float, from_minimum: float, from_maximum: float, to_minimum: float, to_maximum: float) -> float:
+        return (to_maximum - to_minimum) / (from_maximum - from_minimum) * (x - from_minimum) + to_minimum
 
-def scale(x: float, from_minimum: float, from_maximum: float, to_minimum: float, to_maximum: float) -> float:
-    return (to_maximum - to_minimum) / (from_maximum - from_minimum) * (x - from_minimum) + to_minimum
-
-
-def bound(x: float, minimum: float, maximum: float):
-    if x > maximum: return maximum
-    if x < minimum: return minimum
-    return x
+    def bound(x: float, minimum: float, maximum: float):
+        if x > maximum: return maximum
+        if x < minimum: return minimum
+        return x
--- src/fuzzylite/tests/test_operation.py
+++ src/fuzzylite/tests/test_operation.py
@@ -18,47 +18,47 @@
 import math
 import unittest
 
-import fuzzylite.operation as op
+from fuzzylite.operation import Operation as Op
 
 
 class TestOperation(unittest.TestCase):
     def test_validName(self):
-        self.assertEqual(op.valid_name("  xx  "), "xx")  # trims
-        self.assertEqual(op.valid_name("   ~!@#$%^&*()+{}[]:;\"'<>?/,   "), "unnamed")
-        self.assertEqual(op.valid_name("abc123_.ABC"), "abc123_.ABC")
-        self.assertEqual(op.valid_name("      "), "unnamed")
+        self.assertEqual(Op.valid_name("  xx  "), "xx")  # trims
+        self.assertEqual(Op.valid_name("   ~!@#$%^&*()+{}[]:;\"'<>?/,   "), "unnamed")
+        self.assertEqual(Op.valid_name("abc123_.ABC"), "abc123_.ABC")
+        self.assertEqual(Op.valid_name("      "), "unnamed")
 
     def test_str_(self):
-        self.assertEqual(op.str_(0.3), "0.300")
-        self.assertEqual(op.str_(-0.3), "-0.300")
-        self.assertEqual(op.str_(3), "3.000")
-        self.assertEqual(op.str_(3.0001), "3.000")
+        self.assertEqual(Op.str_(0.3), "0.300")
+        self.assertEqual(Op.str_(-0.3), "-0.300")
+        self.assertEqual(Op.str_(3), "3.000")
+        self.assertEqual(Op.str_(3.0001), "3.000")
 
-        self.assertEqual(op.str_(math.inf), "inf")
-        self.assertEqual(op.str_(-math.inf), "-inf")
-        self.assertEqual(op.str_(math.nan), "nan")
+        self.assertEqual(Op.str_(math.inf), "inf")
+        self.assertEqual(Op.str_(-math.inf), "-inf")
+        self.assertEqual(Op.str_(math.nan), "nan")
 
-        op.decimals = 5
-        self.assertEqual(op.str_(0.3), "0.30000")
+        Op.decimals = 5
+        self.assertEqual(Op.str_(0.3), "0.30000")
 
-        op.decimals = 0
-        self.assertEqual(op.str_(0.3), "0")
+        Op.decimals = 0
+        self.assertEqual(Op.str_(0.3), "0")
 
     def test_scale(self):
-        self.assertEqual(op.scale(0, 0, 1, -10, 10), -10.0)
-        self.assertEqual(op.scale(.5, 0, 1, -10, 10), 0.0)
-        self.assertEqual(op.scale(1, 0, 1, -10, 10), 10)
+        self.assertEqual(Op.scale(0, 0, 1, -10, 10), -10.0)
+        self.assertEqual(Op.scale(.5, 0, 1, -10, 10), 0.0)
+        self.assertEqual(Op.scale(1, 0, 1, -10, 10), 10)
 
-        self.assertEqual(op.scale(0, 0, 1, 0, 10), 0.0)
-        self.assertEqual(op.scale(.5, 0, 1, 0, 10), 5.0)
-        self.assertEqual(op.scale(1, 0, 1, 0, 10), 10)
+        self.assertEqual(Op.scale(0, 0, 1, 0, 10), 0.0)
+        self.assertEqual(Op.scale(.5, 0, 1, 0, 10), 5.0)
+        self.assertEqual(Op.scale(1, 0, 1, 0, 10), 10)
 
-        self.assertEqual(op.scale(-1, 0, 1, 0, 10), -10.0)
-        self.assertEqual(op.scale(2, 0, 1, 0, 10), 20)
+        self.assertEqual(Op.scale(-1, 0, 1, 0, 10), -10.0)
+        self.assertEqual(Op.scale(2, 0, 1, 0, 10), 20)
 
-        self.assertEqual(math.isnan(op.scale(math.nan, 0, 1, 0, 10)), True)
-        self.assertEqual(op.scale(math.inf, 0, 1, 0, 10), math.inf)
-        self.assertEqual(op.scale(-math.inf, 0, 1, 0, 10), -math.inf)
+        self.assertEqual(math.isnan(Op.scale(math.nan, 0, 1, 0, 10)), True)
+        self.assertEqual(Op.scale(math.inf, 0, 1, 0, 10), math.inf)
+        self.assertEqual(Op.scale(-math.inf, 0, 1, 0, 10), -math.inf)
 
 
 if __name__ == '__main__':
