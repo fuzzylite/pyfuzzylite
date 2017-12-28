5228efdab79aeb94534cb0a238c3a79d6ceb4bfb
Juan Rada-Vilela <jcrada@fuzzylite.com>
2017-12-28 13:59:51 +1300

    Package imports

---
+++ src/fuzzylite/__init__.py
@@ -0,0 +1,12 @@
+from .engine import *
+from .exporter import *
+from .hedge import *
+from .importer import *
+from .norm import *
+from .operation import *
+from .rule import *
+from .term import *
+from .variable import *
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)-20s - %(levelname)s - %(message)s')
