160c50ff5d28b0d0dcbce1181fc9754a87aebe85
Juan Rada-Vilela <jcrada@fuzzylite.com>
2018-01-02 21:12:54 +1300

    FllExporter: variable, input variable, and output variable

--- fuzzylite/exporter.py
+++ fuzzylite/exporter.py
@@ -29,13 +29,40 @@ class FllExporter(Exporter):
         self.indent = indent
         self.separator = separator
 
-    def input_variable(self, iv: 'InputVariable'):
+    def engine(self, engine: 'Engine') -> str:
+        pass
+
+    def variable(self, v: 'Variable') -> str:
+        result = [f"Variable: {v.name}",
+                  f"{self.indent}description: {v.description}",
+                  f"{self.indent}enabled: {str(v.enabled).lower()}",
+                  f"{self.indent}range: {' '.join([Op.str(v.minimum), Op.str(v.maximum)])}",
+                  f"{self.indent}lock-range: {str(v.enabled).lower()}",
+                  *[f"{self.indent}{self.term(term)}" for term in v.terms]
+                  ]
+        return self.separator.join(result)
+
+    def input_variable(self, iv: 'InputVariable') -> str:
         result = [f"InputVariable: {iv.name}",
                   f"{self.indent}description: {iv.description}",
                   f"{self.indent}enabled: {str(iv.enabled).lower()}",
                   f"{self.indent}range: {' '.join([Op.str(iv.minimum), Op.str(iv.maximum)])}",
                   f"{self.indent}lock-range: {str(iv.enabled).lower()}",
-                  *[f"{self.indent}{str(term)}" for term in iv.terms]
+                  *[f"{self.indent}{self.term(term)}" for term in iv.terms]
+                  ]
+        return self.separator.join(result)
+
+    def output_variable(self, ov: 'OutputVariable') -> str:
+        result = [f"OutputVariable: {ov.name}",
+                  f"{self.indent}description: {ov.description}",
+                  f"{self.indent}enabled: {str(ov.enabled).lower()}",
+                  f"{self.indent}range: {' '.join([Op.str(ov.minimum), Op.str(ov.maximum)])}",
+                  f"{self.indent}lock-range: {str(ov.enabled).lower()}",
+                  f"{self.indent}aggregation: {self.norm(ov.aggregation)}",
+                  f"{self.indent}defuzzifier: {self.defuzzifier(ov.defuzzifier)}",
+                  f"{self.indent}default: {Op.str(ov.default_value)}",
+                  f"{self.indent}lock-previous: {str(ov.lock_previous_value).lower()}",
+                  *[f"{self.indent}{self.term(term)}" for term in ov.terms]
                   ]
         return self.separator.join(result)
 
@@ -45,6 +72,16 @@ class FllExporter(Exporter):
 
     def norm(self, norm: 'Norm') -> str:
         return type(norm).__name__ if norm else "none"
+
+    def defuzzifier(self, defuzzifier: 'Defuzzifier') -> str:
+        if not defuzzifier: return "none"
+        from .defuzzifier import IntegralDefuzzifier, WeightedDefuzzifier
+        result = [defuzzifier.__class__.__name__]
+        if isinstance(defuzzifier, IntegralDefuzzifier):
+            result.append(defuzzifier.resolution)
+        elif isinstance(defuzzifier, WeightedDefuzzifier):
+            result.append(defuzzifier.type)
+        return " ".join(result)
 
     def rule(self, rule: 'Rule') -> str:
         return "rule: %s" % rule.text
