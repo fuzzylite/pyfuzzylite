7a545f164f1da0502194e78e6c852bd35c103645
Juan Rada-Vilela <jcrada@fuzzylite.com>
2018-03-17 11:22:35 +1300

    Defuzzifiers

--- fuzzylite/defuzzifier.py
+++ fuzzylite/defuzzifier.py
@@ -15,20 +15,181 @@
  fuzzylite is a registered trademark of FuzzyLite Limited.
 """
 
-from .term import Term
+from enum import Enum, auto
+from math import isfinite, nan
+
+from .operation import Operation as Op
+from .term import Aggregated, Constant, Function, Linear, Term
 
 
 class Defuzzifier(object):
-    def defuzzify(self, term: Term, minimum: float, maximum: float):
+    def configure(self, parameters: str):
+        raise NotImplementedError()
+
+    def parameters(self):
+        raise NotImplementedError()
+
+    def defuzzify(self, term: Term, minimum: float, maximum: float) -> float:
         raise NotImplementedError()
 
 
 class IntegralDefuzzifier(Defuzzifier):
     __slots__ = "resolution"
 
-    pass
+    default_resolution = 100
+
+    def __init__(self, resolution):
+        self.resolution = resolution if resolution else IntegralDefuzzifier.default_resolution
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.parameters()}"
+
+    def configure(self, parameters: str):
+        if parameters:
+            self.resolution = int(parameters)
+
+    def parameters(self):
+        return Op.str(self.resolution)
+
+    def defuzzify(self, term: Term, minimum: float, maximum: float) -> float:
+        raise NotImplementedError()
+
+
+class BiSector(IntegralDefuzzifier):
+    def __init__(self, resolution=None):
+        super().__init__(resolution)
+
+    def defuzzify(self, term: Term, minimum: float, maximum: float) -> float:
+        if not isfinite(minimum + maximum):
+            return nan
+        resolution = self.resolution
+        dx = (maximum - minimum) / resolution
+        counter = resolution
+        left = right = 0
+        x_left, x_right = (minimum, maximum)
+        left_area = right_area = 0.0
+
+        # TODO: Improve?
+        while counter > 0:
+            counter = counter - 1
+            if left_area <= right_area:
+                x_left = minimum + (left + 0.5) * dx
+                left_area += term.membership(x_left)
+                left += 1
+            else:
+                x_right = maximum - (right + 0.5) * dx
+                right_area += term.membership(x_right)
+                right += 1
+
+        # Inverse weighted average to compensate
+        return (left_area * x_right + right_area * x_left) / (left_area + right_area)
+
+
+class Centroid(IntegralDefuzzifier):
+    def __init__(self, resolution=None):
+        super().__init__(resolution)
+
+    def defuzzify(self, term: Term, minimum: float, maximum: float) -> float:
+        if not isfinite(minimum + maximum):
+            return nan
+        resolution = self.resolution
+        dx = (maximum - minimum) / resolution
+        area = x_centroid = float(0.0)
+        for i in range(0, resolution):
+            x = minimum + (i + 0.5) * dx
+            y = term.membership(x)
+            x_centroid += y * x
+            area += y
+        return x_centroid / area
 
 
 class WeightedDefuzzifier(Defuzzifier):
     __slots__ = "type"
-    pass
+
+    class Type(Enum):
+        Automatic = auto(),
+        TakagiSugeno = auto(),
+        Tsukamoto = auto()
+
+    def __init__(self, type_name: str):
+        self.type = WeightedDefuzzifier.Type[type_name] if type_name else WeightedDefuzzifier.Type.Automatic
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.parameters()}"
+
+    def configure(self, parameters: str):
+        if parameters:
+            self.type = WeightedDefuzzifier.Type[parameters]
+
+    def parameters(self):
+        return self.type.name if self.type else ""
+
+    def defuzzify(self, term: Term, minimum: float, maximum: float) -> float:
+        raise NotImplementedError()
+
+    def infer_type(self, term: Term):
+        if isinstance(term, (Constant, Linear, Function)):
+            return WeightedDefuzzifier.Type.TakagiSugeno
+        return WeightedDefuzzifier.Type.Tsukamoto
+
+
+class WeightedAverage(WeightedDefuzzifier):
+    def __init__(self, defuzz_type: str = None):
+        super().__init__(defuzz_type)
+
+    def defuzzify(self, fuzzy_output: Aggregated, *_):
+        if not fuzzy_output.terms:
+            return nan
+        if not self.type:
+            raise ValueError("expected a type of defuzzifier, but found none")
+
+        this_type = self.type
+        if self.type == WeightedDefuzzifier.Type.Automatic:
+            this_type = self.infer_type(fuzzy_output.terms[0])
+
+        weighted_sum = weights = 0.0
+        if this_type == WeightedDefuzzifier.Type.TakagiSugeno:
+            # Provides Takagi-Sugeno and Inverse Tsukamoto of Functions
+            for activated in fuzzy_output.terms:
+                w = activated.degree
+                z = activated.term.membership(w)
+                weighted_sum += w * z
+                weights += w
+        else:
+            for activated in fuzzy_output.terms:
+                w = activated.degree
+                z = activated.term.tsukamoto(w, fuzzy_output.minimum, fuzzy_output.maximum)
+                weighted_sum += w * z
+                weights += w
+
+        return weighted_sum / weights
+
+
+class WeightedSum(WeightedDefuzzifier):
+    def __init__(self, defuzz_type: str = None):
+        super().__init__(defuzz_type)
+
+    def defuzzify(self, fuzzy_output: Aggregated, *_):
+        if not fuzzy_output.terms:
+            return nan
+        if not self.type:
+            raise ValueError("expected a type of defuzzifier, but found none")
+
+        this_type = self.type
+        if self.type == WeightedDefuzzifier.Type.Automatic:
+            this_type = self.infer_type(fuzzy_output.terms[0])
+
+        weighted_sum = 0.0
+        if this_type == WeightedDefuzzifier.Type.TakagiSugeno:
+            # Provides Takagi-Sugeno and Inverse Tsukamoto of Functions
+            for activated in fuzzy_output.terms:
+                w = activated.degree
+                z = activated.term.membership(w)
+                weighted_sum += w * z
+        else:
+            for activated in fuzzy_output.terms:
+                w = activated.degree
+                z = activated.term.tsukamoto(w, fuzzy_output.minimum, fuzzy_output.maximum)
+                weighted_sum += w * z
+
+        return weighted_sum
---
+++ tests/test_defuzzifier.py
@@ -0,0 +1,237 @@
+"""
+ pyfuzzylite (TM), a fuzzy logic control library in Python.
+ Copyright (C) 2010-2017 FuzzyLite Limited. All rights reserved.
+ Author: Juan Rada-Vilela, Ph.D. <jcrada@fuzzylite.com>
+
+ This file is part of pyfuzzylite.
+
+ pyfuzzylite is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
+
+ You should have received a copy of the FuzzyLite License along with
+ pyfuzzylite. If not, see <http://www.fuzzylite.com/license/>.
+
+ pyfuzzylite is a trademark of FuzzyLite Limited
+ fuzzylite is a registered trademark of FuzzyLite Limited.
+"""
+
+import unittest
+
+from fuzzylite import *
+
+
+class DefuzzifierAssert(object):
+    def __init__(self, test: unittest.TestCase, actual: Defuzzifier):
+        self.test = test
+        self.actual = actual
+        self.test.maxDiff = None  # show all differences
+
+    def exports_fll(self, fll: str):
+        self.test.assertEqual(str(self.actual), fll)
+        return self
+
+    def configured_as(self, parameters: str):
+        self.actual.configure(parameters)
+        return self
+
+    def has_parameters(self, parameters: str):
+        self.test.assertEqual(self.actual.parameters(), parameters)
+        return self
+
+    def defuzzifies(self, terms: Dict[Term, float], minimum=-inf, maximum=inf):
+        for term, result in terms.items():
+            if isnan(result):
+                self.test.assertEqual(isnan(self.actual.defuzzify(term, minimum, maximum)), True, f"for {str(term)}")
+            else:
+                self.test.assertAlmostEqual(self.actual.defuzzify(term, minimum, maximum), result, places=15,
+                                            msg=f"for {str(term)}")
+        return result
+
+
+class TestDefuzzifier(unittest.TestCase):
+    def test_defuzzifier(self):
+        with self.assertRaises(NotImplementedError):
+            Defuzzifier().configure("")
+        with self.assertRaises(NotImplementedError):
+            Defuzzifier().parameters()
+        with self.assertRaises(NotImplementedError):
+            Defuzzifier().defuzzify(None, nan, nan)
+
+    def test_integral_defuzzifier(self):
+        DefuzzifierAssert(self, IntegralDefuzzifier(100)) \
+            .exports_fll("IntegralDefuzzifier 100") \
+            .has_parameters("100") \
+            .configured_as("300") \
+            .exports_fll("IntegralDefuzzifier 300")
+        with self.assertRaises(NotImplementedError):
+            IntegralDefuzzifier(nan).defuzzify(None, nan, nan)
+
+    def test_bisector(self):
+        DefuzzifierAssert(self, BiSector()) \
+            .exports_fll("BiSector 100") \
+            .has_parameters("100") \
+            .configured_as("200") \
+            .exports_fll("BiSector 200")
+
+        DefuzzifierAssert(self, BiSector()) \
+            .defuzzifies(
+            {
+                # Triangle("", -1, 0): -0.5,
+                # Triangle("", -1, 1): 0.0,
+                # Triangle("", 0, 1): 0.5,
+                Triangle("", -1, -1, 0): -0.5,
+                Triangle("", -1, 1, 2): 0.0,
+                Triangle("", 0, 0, 3): 0.5,
+                Aggregated("", 0, 1, Maximum(),
+                           [Activated(Triangle("Medium", 0.25, 0.5, 0.75), 0.2, Minimum()),
+                            Activated(Triangle("High", 0.5, 0.75, 1.0), 0.8, Minimum())])
+                : 0.7200552486187846
+            }, -1, 0)
+
+    def test_centroid(self):
+        DefuzzifierAssert(self, Centroid()) \
+            .exports_fll("Centroid 100") \
+            .has_parameters("100") \
+            .configured_as("200") \
+            .exports_fll("Centroid 200")
+
+        DefuzzifierAssert(self, Centroid()) \
+            .defuzzifies(
+            {
+                Triangle("", -1, 0): -0.5,
+                Triangle("", -1, 1): 0.0,
+                Triangle("", 0, 1): 0.5,
+                Aggregated("", 0, 1, Maximum(),
+                           [Activated(Triangle("Medium", 0.25, 0.5, 0.75), 0.2, Minimum()),
+                            Activated(Triangle("High", 0.5, 0.75, 1.0), 0.8, Minimum())])
+                : 0.6900552486187845
+            }, -1, 1)
+
+    def test_weighted_defuzzifier(self):
+        self.assertEqual(WeightedDefuzzifier(None).type, WeightedDefuzzifier.Type.Automatic)
+        self.assertEqual(WeightedDefuzzifier("TakagiSugeno").type, WeightedDefuzzifier.Type.TakagiSugeno)
+
+        defuzzifier = WeightedDefuzzifier(None)
+        defuzzifier.configure("TakagiSugeno")
+        self.assertEqual(defuzzifier.type, WeightedDefuzzifier.Type.TakagiSugeno)
+
+        defuzzifier.type = None
+        defuzzifier.configure("")
+        self.assertEqual(defuzzifier.type, None)
+
+        with self.assertRaises(KeyError):
+            defuzzifier.configure("ABC")
+
+        with self.assertRaises(NotImplementedError):
+            defuzzifier.defuzzify(None, nan, nan)
+
+        self.assertEqual(defuzzifier.infer_type(Constant()), WeightedDefuzzifier.Type.TakagiSugeno)
+        self.assertEqual(defuzzifier.infer_type(Triangle()), WeightedDefuzzifier.Type.Tsukamoto)
+
+    def test_weighted_average(self):
+        DefuzzifierAssert(self, str(WeightedAverage())).exports_fll("WeightedAverage Automatic")
+        DefuzzifierAssert(self, str(WeightedAverage("TakagiSugeno"))).exports_fll("WeightedAverage TakagiSugeno")
+        with self.assertRaises(KeyError):
+            DefuzzifierAssert(self, str(WeightedAverage("SugenoTakagi")))
+
+        defuzzifier = WeightedAverage()
+        defuzzifier.type = None
+        with self.assertRaisesRegex(ValueError, "expected a type of defuzzifier, but found none"):
+            defuzzifier.defuzzify(Aggregated(terms=[None]))
+        with self.assertRaisesRegex(AttributeError, "'Triangle' object has no attribute 'terms'"):
+            defuzzifier.defuzzify(Triangle())
+
+        DefuzzifierAssert(self, WeightedAverage()) \
+            .defuzzifies({Aggregated(): nan,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 2.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 0.5),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 2.0,
+                          Aggregated(terms=[Activated(Constant("", -1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 0.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", -3.0), 0.5),
+                                            ]): -1.0
+                          })
+        DefuzzifierAssert(self, WeightedAverage("Tsukamoto")) \
+            .defuzzifies({Aggregated(): nan,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 2.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 0.5),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 2.0,
+                          Aggregated(terms=[Activated(Constant("", -1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 0.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", -3.0), 0.5),
+                                            ]): -1.0
+                          })
+
+    def test_weighted_sum(self):
+        DefuzzifierAssert(self, str(WeightedSum())).exports_fll("WeightedSum Automatic")
+        DefuzzifierAssert(self, str(WeightedSum("TakagiSugeno"))).exports_fll("WeightedSum TakagiSugeno")
+        with self.assertRaises(KeyError):
+            DefuzzifierAssert(self, str(WeightedSum("SugenoTakagi")))
+
+        defuzzifier = WeightedSum()
+        defuzzifier.type = None
+        with self.assertRaisesRegex(ValueError, "expected a type of defuzzifier, but found none"):
+            defuzzifier.defuzzify(Aggregated(terms=[None]))
+        with self.assertRaisesRegex(AttributeError, "'Triangle' object has no attribute 'terms'"):
+            defuzzifier.defuzzify(Triangle())
+
+        DefuzzifierAssert(self, WeightedSum()) \
+            .defuzzifies({Aggregated(): nan,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 6.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 0.5),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 5.0,
+                          Aggregated(terms=[Activated(Constant("", -1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 0.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", -3.0), 0.5),
+                                            ]): -2.5
+                          })
+        DefuzzifierAssert(self, WeightedSum("Tsukamoto")) \
+            .defuzzifies({Aggregated(): nan,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 6.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", 2.0), 0.5),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 5.0,
+                          Aggregated(terms=[Activated(Constant("", -1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", 3.0), 1.0),
+                                            ]): 0.0,
+                          Aggregated(terms=[Activated(Constant("", 1.0), 1.0),
+                                            Activated(Constant("", -2.0), 1.0),
+                                            Activated(Constant("", -3.0), 0.5),
+                                            ]): -2.5
+                          })
+
+
+if __name__ == '__main__':
+    unittest.main()
