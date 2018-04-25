27cf77805e12e9261480e001c4adbc3e387c05b8
Juan Rada-Vilela <jcrada@fuzzylite.com>
2018-04-25 21:11:47 +1200

    Deleted old sources

--- src/fl/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(level = logging.INFO,
-                    format = '%(asctime)s - %(name)-20s - %(levelname)s - %(message)s')
-
-from .term import *
-from .exporter import *
-from .engine import *
