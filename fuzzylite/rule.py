abf7f899567e29a2b6c9f21d4892ce3e928f82f7
Juan Rada-Vilela <jcrada@fuzzylite.com>
2018-04-25 21:21:14 +1200

    Progress on rules

--- fuzzylite/rule.py
+++ fuzzylite/rule.py
@@ -16,50 +16,211 @@
 """
 
 import logging
+from math import nan
 from typing import Iterable
 
-from .activation import Activation
-from .norm import SNorm, TNorm
 from .exporter import FllExporter
+from .hedge import Any
+from .norm import SNorm, TNorm
+from .operation import Operation as Op
+from .variable import InputVariable, OutputVariable
+
+
+class Expression(object):
+    pass
+
+
+class Proposition(Expression):
+    __slots__ = ["variable", "hedges", "term"]
+
+    def __init__(self):
+        self.variable = None
+        self.hedges = []
+        self.term = None
+
+    def __str__(self):
+        result = []
+
+        result.append(self.variable.name if self.variable else "?")
+
+        result.append(Rule.IS)
+
+        if self.hedges:
+            for hedge in self.hedges:
+                result.append(hedge.name)
+
+        result.append(self.term.name if self.term else "?")
+
+        return " ".join(result)
+
+
+class Operator(Expression):
+    __slots__ = ["name", "left", "right"]
+
+    def __init__(self):
+        self.name = ""
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return self.name
+
+
+class Antecedent(object):
+    __slots__ = ["text", "expression"]
+
+    def __init__(self):
+        self.text = ""
+        self.expression = None
+
+    def is_loaded(self) -> bool:
+        return bool(self.expression)
+
+    def activation_degree(self, conjunction: TNorm, disjunction: SNorm) -> float:
+        if not self.is_loaded():
+            raise ValueError(f"antecedent <{self.text}> is not loaded")
+
+        return self._activation_degree(conjunction, disjunction, self.expression)
+
+    def _activation_degree(self, conjunction: TNorm, disjunction: SNorm, node: Expression):
+        if not node:
+            raise ValueError("expected an expression node, but found none")
+
+        if isinstance(node, Proposition):
+            proposition = node
+            if not proposition.variable.enabled:
+                return 0.0
+
+            if proposition.hedges:
+                # if last hedge is "Any", apply hedges in reverse order and return degree
+                if isinstance(proposition.hedges[-1], Any):
+                    result = nan
+                    for hedge in reversed(proposition.hedges):
+                        result = hedge.hedge(result)
+                    return result
+
+            result = nan
+            if isinstance(proposition.variable, InputVariable):
+                result = node.term.membership(proposition.variable.value)
+            elif isinstance(proposition.variable, OutputVariable):
+                result = node.variable.fuzzy.activation_degree(proposition.term)
+
+            if proposition.hedges:
+                for hedge in reversed(proposition.hedges):
+                    result = hedge.hedge(result)
+
+            return result
+
+        if isinstance(node, Operator):
+            operator = node
+            if not (operator.left and operator.right):
+                raise ValueError("expected left and right operands")
+
+            if operator.name == Rule.AND:
+                if not conjunction:
+                    raise ValueError(f"rule requires a conjunction operator: '{self.text}'")
+                return conjunction.compute(self._activation_degree(conjunction, disjunction, operator.left),
+                                           self._activation_degree(conjunction, disjunction, operator.right))
+
+            if operator.name == Rule.OR:
+                if not disjunction:
+                    raise ValueError(f"rule requires a disjunction operator: '{self.text}'")
+                return disjunction.compute(self._activation_degree(conjunction, disjunction, operator.left),
+                                           self._activation_degree(conjunction, disjunction, operator.right))
+
+            raise ValueError(f"operator <{operator.name}> not recognized")
+
+        raise ValueError(f"expected a Proposition or an Operator, but found <{str(node)}>")
+
+    def unload(self):
+        self.expression = None
+
+    def load(self, engine: 'Engine'):
+        self.load(self.text, engine)
+
+    def load(self, antecedent: str, engine: 'Engine'):
+        logging.debug(f"Antecedent: {antecedent}")
+        self.unload()
+        self.text = antecedent
+        if not antecedent:
+            raise ValueError("expected the antecedent of a rule, but found none")
+
+
+class Consequent(object):
+    pass
 
 
 class Rule(object):
-    '''Defines a fuzzy rule'''
+    __slots__ = ["enabled", "weight", "activation_degree", "triggered", "antecedent", "consequent"]
 
-    FR_IF = 'if'
-    FR_IS = 'is'
-    FR_THEN = 'then'
-    FR_AND = 'and'
-    FR_OR = 'or'
-    FR_WITH = 'with'
+    IF = 'if'
+    IS = 'is'
+    THEN = 'then'
+    AND = 'and'
+    OR = 'or'
+    WITH = 'with'
 
-    def __init__(self, text: str):
+    def __init__(self):
+        self.enabled = True
+        self.weight = 1.0
+        self.activation_degree = 0.0
+        self.triggered = False
         self.antecedent = None
         self.consequent = None
-        self.text = text
-        self.logger = logging.getLogger(type(self).__name__)
 
-    def configure(self, fop):
+    def text(self) -> str:
+        result = [Rule.IF]
+        if self.antecedent:
+            result.append(self.antecedent.text)
+        result.append(Rule.THEN)
+        if self.consequent:
+            result.append(self.consequent.text)
+        if self.weight != 1.0:
+            result.append(Rule.WITH)
+            result.append(Op.str(self.weight))
+        return " ".join(result)
+
+    def deactivate(self):
         pass
 
-    def firing_strength(self, tnorm, snorm):
-        return self.antecedent.firing_strength(tnorm, snorm)
+    def activate_with(self, conjunction: TNorm, disjunction: SNorm):
+        pass
 
-    def fire(self, strength, activation):
-        self.consequent.fire(strength, activation)
+    def trigger(self, implication: TNorm):
+        pass
 
-    def __str__(self):
-        return '%s %s %s %s' % (Rule.FR_IF, str(self.antecedent),
-                                Rule.FR_THEN, str(self.consequent))
+    def is_triggered(self) -> bool:
+        return False
+
+    def is_loaded(self) -> bool:
+        return (self.antecedent and self.consequent and
+                self.antecedent.is_loaded() and self.consequent.is_loaded())
+
+    def unload(self) -> None:
+        self.deactivate()
+        if self.antecedent: self.antecedent.unload()
+        if self.consequent: self.consequent.unload()
+
+    def load(self, rule: str, engine: "Engine"):
+        pass
+
+    def load(self, rule: str):
+        pass
+
+    @staticmethod
+    def parse(text: str, engine: "Engine") -> "Rule":
+        rule = Rule()
+        rule.load(text, engine)
+        return rule
 
 
 class RuleBlock(object):
-    __slots__ = "name", "description", "enabled", "conjunction", "disjunction", "implication", "activation", "rules"
+    __slots__ = ["name", "description", "enabled", "conjunction", "disjunction", "implication", "activation", "rules"]
 
     def __init__(self, name: str = "", description: str = "", enabled: bool = True,
                  conjunction: TNorm = None, disjunction: SNorm = None,
-                 implication: TNorm = None, activation: Activation = None,
-                 rules: Iterable[Rule] = None):
+                 implication: TNorm = None, activation: "Activation" = None,
+                 rules: Iterable["Rule"] = None):
         self.name = name
         self.description = description
         self.enabled = enabled
@@ -70,7 +231,6 @@ class RuleBlock(object):
         self.rules = []
         if rules:
             self.rules.extend(rules)
-
 
     def __str__(self):
         return FllExporter().rule_block(self)
--- tests/test_rule.py
+++ tests/test_rule.py
@@ -20,5 +20,67 @@ import unittest
 from fuzzylite import *
 from tests.assert_component import ComponentAssert
 
+
 class RuleBlockAssert(ComponentAssert):
     pass
+
+
+class TestRuleBlock(unittest.TestCase):
+    def test_constructor(self):
+        RuleBlockAssert(self, RuleBlock()) \
+            .exports_fll("\n".join(
+                ["RuleBlock: ",
+                 "  description: ",
+                 "  enabled: true",
+                 "  conjunction: none",
+                 "  disjunction: none",
+                 "  implication: none",
+                 "  activation: none",
+                 ]))
+
+        RuleBlockAssert(self, RuleBlock("rb", "a ruleblock", enabled=False,
+                                        conjunction=TNorm(), disjunction=SNorm(),
+                                        implication=TNorm(), activation=Activation(),
+                                        rules=[Rule("if a then z"), Rule("if b then y")])) \
+            .exports_fll("\n".join(
+                ["RuleBlock: rb",
+                 "  description: a ruleblock",
+                 "  enabled: false",
+                 "  conjunction: TNorm",
+                 "  disjunction: SNorm",
+                 "  implication: TNorm",
+                 "  activation: Activation",
+                 "  rule: if a then z",
+                 "  rule: if b then y",
+                 ]))
+
+class TestExpression(unittest.TestCase):
+    def test_expression(self):
+        with self.assertRaises(NotImplementedError):
+            Expression().type()
+
+    def test_proposition(self):
+        self.assertEqual(Proposition().type(), Expression.Type.Proposition)
+        self.assertEqual(str(Proposition()), "? is ?")
+
+        proposition = Proposition()
+        proposition.variable = Variable("variable")
+        proposition.hedges = [Very()]
+        proposition.term = Term("term")
+
+        self.assertEqual(str(proposition), "variable is very term")
+
+    def test_operator(self):
+        self.assertEqual(Operator().type(), Expression.Type.Operator)
+        self.assertEqual(Operator().name, "")
+
+        operator = Operator()
+        operator.name = "operator"
+        operator.left = "left"
+        operator.right = "right"
+        self.assertEqual(str(operator), "operator")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
