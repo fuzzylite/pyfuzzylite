5d822ed6165b7c44efa7795b9e003b292a9a5f74
Juan Rada-Vilela <jcrada@fuzzylite.com>
2019-04-07 10:54:39 +1200

    examples inside package

---
+++ fuzzylite/examples/terms/SigmoidProduct.py
@@ -0,0 +1,52 @@
+import fuzzylite as fl
+
+engine = fl.Engine(
+    name="SigmoidProduct",
+    description="obstacle avoidance for self-driving cars"
+)
+
+obstacle = fl.InputVariable(
+    name="obstacle",
+    description="location of obstacle relative to vehicle",
+    enabled=True,
+    minimum=0.000000000,
+    maximum=1.000000000,
+    lock_range=False,
+    terms=[
+        fl.Triangle("left", 0.000000000, 0.333000000, 0.666000000),
+        fl.Triangle("right", 0.333000000, 0.666000000, 1.000000000)
+    ]
+)
+engine.input_variables = [obstacle]
+
+steer = fl.OutputVariable(
+    name="steer",
+    description="direction to steer the vehicle to",
+    enabled=True,
+    minimum=0.000000000,
+    maximum=1.000000000,
+    lock_range=False,
+    aggregation=fl.Maximum(),
+    defuzzifier=fl.Centroid(100),
+    lock_previous=False,
+    terms=[
+        fl.SigmoidProduct("left", 0.166500000, 30.030030030, -30.030030030, 0.499500000),
+        fl.SigmoidProduct("right", 0.499750000, 29.985007496, -29.985007496, 0.833250000)
+    ]
+)
+engine.output_variables = [steer]
+
+steer_away = fl.RuleBlock(
+    name="steer_away",
+    description="steer away from obstacles",
+    enabled=True,
+    conjunction=None,
+    disjunction=None,
+    implication=fl.Minimum(),
+    activation=fl.General(),
+    rules=[
+        fl.Rule.parse("if obstacle is left then steer is right"),
+        fl.Rule.parse("if obstacle is right then steer is left")
+    ]
+)
+engine.rule_blocks = [steer_away]
---
+++ fuzzylite/examples/terms/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["Bell", "Binary", "Concave", "Constant", "Cosine", "Discrete", "Function", "Gaussian",
+           "GaussianProduct", "Linear", "PiShape", "Ramp", "Rectangle", "Sigmoid",
+           "SigmoidDifference", "SigmoidProduct", "Spike", "Trapezoid", "Triangle", "ZSShape", ]
