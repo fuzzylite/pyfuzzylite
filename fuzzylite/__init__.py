0466074fb8b275df5c49ff7ede33038cc1a7bbbe
Juan Rada-Vilela <jcrada@gmail.com>
2023-11-23 09:55:22 +1100

    License

--- fuzzylite/__init__.py
+++ fuzzylite/__init__.py
@@ -11,7 +11,8 @@ the terms of the FuzzyLite License included with the software.
 You should have received a copy of the FuzzyLite License along with
 pyfuzzylite. If not, see <https://github.com/fuzzylite/pyfuzzylite/>.
 
-pyfuzzylite is a trademark of FuzzyLite Limited
+pyfuzzylite is a trademark of FuzzyLite Limited.
+
 fuzzylite is a registered trademark of FuzzyLite Limited.
 """
 from . import examples  # noqa
