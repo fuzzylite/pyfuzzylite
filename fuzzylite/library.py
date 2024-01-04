d36f2b7eb7063a527f6a210a5a87622885bcd3d7
Juan Rada-Vilela <jcrada@gmail.com>
2024-01-04 16:37:45 +1100

    8.0.1

--- fuzzylite/library.py
+++ fuzzylite/library.py
@@ -272,7 +272,7 @@ class Information:
         Returns:
             version of the library
         """
-        __version__ = "8.0.0"
+        __version__ = "8.0.1"
         return __version__
 
 
